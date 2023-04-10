a30ad2a23b143a7752c1672e7bf671416edb7bd7
Muhammad Rasel Bin Siddik <muhammadraselbinsiddik@gmail.com>
2023-04-10 12:47:45 +0600

    data create

--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { CurrencyDollarIcon, MapPinIcon } from '@heroicons/react/24/solid'
 
 const Job = ({ job }) => {
-    const { id, title, name, logo, Contact_information, loction, salary } = job;
+    const { id, title, name, logo, Contact_information, location, salary } = job;
     return (
         <div>
-            
+            <img src={logo} alt="" />
+            <div>
+                {title}
+                {name}
+                <button>Remote</button><button>full time</button>
+                <span><MapPinIcon class="h-6 w-6 text-blue-300" />{location}</span> <span><CurrencyDollarIcon class="h-6 w-6 text-blue-300" />{salary}</span>
+            </div>
         </div>
     );
 };
